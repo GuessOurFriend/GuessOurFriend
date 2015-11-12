cebc697f3f8c1c9359432f13b7bc9f2400657458
erickoth <ekoth@wisc.edu>
2015-11-11 21:25:47 -0600

    Extracted out a NetworkRequestGetJSONRunner class that can be used to make a request to a URL and parse the result as JSON. This allows network requests to be made asynchronously with ease. I modeled it after code already written for the MatchmakingController and then implemeneted it there.

--- app/src/main/java/inc/guessourfriend/MatchmakingController.java
+++ app/src/main/java/inc/guessourfriend/MatchmakingController.java
@@ -52,66 +52,37 @@ public class MatchmakingController extends SlideNavigationController {
         return super.onOptionsItemSelected(item);
     }
 
-    //Create an AsyncTaskRunner class to run the network stuff on a separate thread
-    private class AsyncTaskRunner extends AsyncTask<String, String, String> {
+    public void onBeginMatchmaking(View view) {
+        //Disable the button while we handle the request
+        Button button = (Button) findViewById(R.id.find_a_game_button);
+        button.setEnabled(false);
 
-        @Override
-        protected String doInBackground(String... params) {
-            //Create a URL object pointing to the location of our method
-            URL url = null;
-            try {
-                //TODO: Point this at our server when it's running
-                url = new URL("http://jsonplaceholder.typicode.com/posts/1");
-            } catch (MalformedURLException ex) {
-                //This can't really happen unless we mess up, but AndroidStudio will complain without it
-                ex.printStackTrace();
-            }
-
-            String result = null;
-
-            //Make the request
-            HttpURLConnection urlConnection = null;
-            try {
-                //Open the connection and convert the result to a string
-                urlConnection = (HttpURLConnection) url.openConnection();
-                BufferedReader reader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
-                StringBuilder builder = new StringBuilder();
-                String line;
-                while ((line = reader.readLine()) != null) {
-                    builder.append(line + "\n");
-                }
-                reader.close();
-                result = builder.toString();
-            } catch (IOException ex) {
-                ex.printStackTrace();
-                //TODO: Handle this error more appropriately
-            } finally {
-                if (urlConnection != null) {
-                    urlConnection.disconnect();
-                }
-            }
-
-            return result;
-        }
-
-        @Override
-        protected void onPostExecute(String result) {
-            Long facebookId = null;
-
-            //Parse the result as JSON
-            JSONObject jsonResult = null;
-            try {
-                jsonResult = new JSONObject(result.toString());
-
-                //Get the facebook id
+        //Request a member from the queue asynchronously
+        //TODO: Point this at our server when it's running
+        new NetworkRequestGetJSONRunner("http://jsonplaceholder.typicode.com/posts/1") {
+            @Override
+            protected void onPostExecute(JSONObject result) {
                 //TODO: Change to parse whatever our model returns
-                facebookId = null;//jsonResult.getLong("userId");
+                //Get the facebook id
+                Long facebookId = null;
+                try {
+                    facebookId = result.getLong("userId");
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
 
                 //Check if there was a friend in the queue
                 if (facebookId != null) {
                     //Create a game with this friend
 
-                    //TODO: Send to StartOfGameController once it exists
+                    //TODO: Send to StartOfGameController once it exists and remove this test code
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            Button button = (Button) findViewById(R.id.find_a_game_button);
+                            button.setText("Starting Match...");
+                        }
+                    });
                 } else {
                     //TODO: Add this user to the queue via a POST
 
@@ -125,20 +96,7 @@ public class MatchmakingController extends SlideNavigationController {
                         }
                     });
                 }
-            } catch (JSONException ex) {
-                //This shouldn't happen unless we mess up
-                ex.printStackTrace();
             }
-        }
-    }
-
-    public void onBeginMatchmaking(View view) {
-        //Disable the button while we handle the request
-        Button button = (Button) findViewById(R.id.find_a_game_button);
-        button.setEnabled(false);
-
-        //Request the queue asynchronously
-        AsyncTaskRunner runner = new AsyncTaskRunner();
-        runner.execute();
+        }.execute();
     }
 }
