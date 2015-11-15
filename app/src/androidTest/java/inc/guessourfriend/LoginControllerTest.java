0dc2128f2d4e672ed604e418927e2f0a509d1233
Stephen Ellmaurer <stellmaurer@gmail.com>
2015-11-15 17:23:23 -0600

    Tests now work.

--- app/src/androidTest/java/inc/guessourfriend/LoginControllerTest.java
+++ app/src/androidTest/java/inc/guessourfriend/LoginControllerTest.java
@@ -1,10 +1,12 @@
 package inc.guessourfriend;
 
+import android.test.AndroidTestCase;
+
 import junit.framework.Assert;
 
 import org.junit.Test;
 
-public class LoginControllerTest {
+public class LoginControllerTest extends AndroidTestCase {
 
     @Test
     public void abc_returnsTrue() {
