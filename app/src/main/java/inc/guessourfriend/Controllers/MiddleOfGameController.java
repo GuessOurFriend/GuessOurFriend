e54f4d5b5b4abb87a7444a8a3d7ec9fabc090e89
erickoth <ekoth@wisc.edu>
2015-12-07 10:32:38 -0600

    Fixed a defect with accidently putting the body in the title area. Also updated the turn after receiving an answer.

--- app/src/main/java/inc/guessourfriend/Controllers/MiddleOfGameController.java
+++ app/src/main/java/inc/guessourfriend/Controllers/MiddleOfGameController.java
@@ -75,10 +75,13 @@ public class MiddleOfGameController extends SlideNavigationController implements
                                 game.isMyTurn = true;
                                 updateTurnTextViews();
                             } else {
-                                // You need to ask the next question
+                                // You just received an answer,
+                                // You need to guess a mystery friend or pass the guess
                                 int intAnswer = Integer.parseInt(jsonObjectBody.getString("answer"));
                                 String answer = NetworkRequestHelper.intAnswerToString(intAnswer);
                                 conversation.append(answer + "\n");
+                                game.isMyTurn = true;
+                                updateTurnTextViews();
                             }
                         }else if(title.equals("It is your turn")){
                             // The other user just passed on their guess
